7dda2f0d2d6d82734b8e252ea1da500c5ecb92ea
porterhaus <bporter@porterhaus.com>
2015-06-13 17:52:26 -0500

    Bundle installed. Set up devise gem and configured routes.

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  ## Active record relationships
+
+  ## Validations
+
+  ## Callbacks
+  before_create :set_auth_token
+
+  ## Roles
+  #  Possibilities: { patient, caregiver, friend/follower }
+  #  This will be an open when creating a new User.
+  # => def set_user_role
+  # =>     self.role = "patient"
+  # => end
+
+  def set_auth_token
+    self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+    loop do
+      token = SecureRandom.hex
+      break token unless self.class.exists?(auth_token: token)
+    end
+  end
+
+end
