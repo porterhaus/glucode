f6fb72d4ca3948adedae8c6058c8ce0fe05ec0cb
porterhaus <bporter@porterhaus.com>
2015-07-25 14:11:28 -0500

    Revised routing.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   ## Devise mappings
   # => Docs state that you can define more than one model. Is that necessary?
   # devise_for :users, :controllers => {} <= This conflicts with the mapping for the API.
-
   devise_for :users, :skip => [:sessions]
 
   root to: 'site#index'
