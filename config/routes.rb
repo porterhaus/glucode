71c56b249e1e811ae8e9b16d4156f285ed6686ea
porterhaus <bporter@porterhaus.com>
2015-08-20 16:28:51 -0500

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
     namespace :auth do
       get 'signin'
       get 'signout'
-      get 'register' => 'registrations#create', as: :register
     end
     resources :users, :only => [:create, :update, :destroy, :show]
     resources :bgtests
