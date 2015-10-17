72890914bfcd8d31bf48b0829cdf5591332a8674
porterhaus <bporter@porterhaus.com>
2015-10-17 15:40:43 -0500

    Updated seed files for heroku.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require 'faker'
-
 u = User.new(
     :name => "D. Brian Porter",
     :email => "porter4487@gmail.com",
@@ -7,61 +5,3 @@ u = User.new(
     :role => ["Admin"].sample
 )
 u.save
-
-5.times do
-  User.create!(
-    name: Faker::Name.name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password(8),
-    role: ["Patient", "Caregiver", "Friend"].sample
-  )
-end
-users = User.all
-
-500.times do
-    # Add blood glucose test
-    bgtest = Bgtest.create!(
-      value: [Faker::Number.number(2), Faker::Number.number(3)].sample,
-      time_of_day: ["Before breakfast",
-                     "After breakfast",
-                     "Before lunch",
-                     "After lunch",
-                     "Before dinner",
-                     "After dinner",
-                     "Snack"].sample,
-      category: ["Control",
-                 "Pre-Meal",
-                 "Post-Meal",
-                 "Bolus Skip",
-                 "Meal Skip",
-                 "Carb ???",
-                 "Exercise",
-                 "Sick",
-                 "Fasting",
-                 "Pre-Bed"].sample,
-      comments: Faker::Lorem.paragraph,
-      user: users.sample
-    )
-    bgtest.update_attribute(:created_at, (rand*10).weeks.ago)
-    bgtest.save
-
-    # Add carbohydrates or Meal data
-    meal = Meal.create!(
-      name: ["Breakfast", "Lunch", "Dinner", "Snack"].sample,
-      carbohydrates: [Faker::Number.number(2), Faker::Number.number(3)].sample,
-      description: Faker::Lorem.paragraph,
-      user: users.sample
-    )
-    meal.update_attribute(:created_at, (rand*10).weeks.ago)
-    meal.save
-
-    # Add bolus data
-    injection = Injection.create!(
-      num_of_units_taken: [Faker::Number.number(1), Faker::Number.number(2)].sample,
-      category: ["Basal", "Bolus"].sample,
-      user: users.sample
-    )
-    injection.update_attribute(:created_at, (rand*10).weeks.ago)
-    injection.save
-
-end
